8189e3d4493ceff8f9b808847391771cc696c0a8
NoziphoDlamini <dlamininoziphononhlanhla@gmail.com>
2023-05-12 04:12:31 +0200

    Update push.js

--- push.js
+++ push.js
@@ -6,6 +6,6 @@ let vapIDKeys = {
   }
 push.setVapidDetails('mailto:dlamininoziphononhlanhla@gmail.com', vapIDKeys.publicKey , vapIDKeys.privateKey)
 
-let sub ={}
+let sub ={"endpoint":"https://fcm.googleapis.com/fcm/send/cWOdPYtIk14:APA91bHwrVDQxBwXXf-Q3Agkrkrb_fSHFUpsjZ7G-D5WJwqwD5EmDIRjSe_I6e71BGSufZFzySQK34LVpYyX1cpd-s1vSM0Z7sCukF5axes5HUS_Mq-0zugTIPM9nS7YOC4X3IhK4D4l","expirationTime":null,"keys":{"p256dh":"BD47p4hQFFgYtxn4YksRe7YnXkMu7gufnVy_3e335Vjzj-y9qRH-7YsUbCMaG1CRIYZ841NWxio_dGRyzva80dg","auth":"N57a4Y1ROVWEm1P9zgGJWg"}}
 
 push.sendNotification(sub, 'test message')
